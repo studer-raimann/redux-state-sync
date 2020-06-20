57ff25dd879e94b296eda3f96a1608e76f572994
Nicolas Schäfli <ns@studer-raimann.ch>
2020-06-21 01:48:15 +0200

    refactor: update typescript definitions
    Use generics instead of any or unknown where possible.

--- src/syncState.d.ts
+++ src/syncState.d.ts
@@ -3,22 +3,21 @@
 // Definitions by: MU AOHUA <https://github.com/AOHUA>
 //                 AntonioMendez <https://github.com/AntonioMendez>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.4
 
 // Source: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/redux-state-sync/index.d.ts
 
-import { Store, Reducer, Middleware, AnyAction } from "redux";
+import { Store, Reducer, Middleware, Action } from "redux";
 
 export interface Stamp {
     $uuid: string;
     $wuid: string;
     $isSync: boolean;
 }
-export type StampedAction = Stamp & AnyAction;
+export type StampedAction = Stamp & Action;
 
 export interface Config {
     channel?: string;
-    predicate?: (action: AnyAction) => boolean | null;
+    predicate?: (action: Action) => boolean | null;
     blacklist?: Array<string>;
     whitelist?: Array<string>;
     prepareState?: <T, R>(state: T) => R;
@@ -26,18 +25,18 @@ export interface Config {
 
 export interface MessageListenerConfig {
     channel: BroadcastChannel;
-    dispatch: (action: AnyAction | StampedAction) => void;
-    allowed: (action: AnyAction) => boolean;
+    dispatch: (action: Action | StampedAction) => void;
+    allowed: (action: Action) => boolean;
 }
 
-export function generateUuidForAction(action: AnyAction): StampedAction;
+export function generateUuidForAction(action: Action): StampedAction;
 export function isActionAllowed(config: Config): (type: string) => boolean;
 export function createMessageListener(config: MessageListenerConfig): void;
 export function createStateSyncMiddleware(config?: Config): Middleware;
-export function withReduxStateSync(
-    appReducer: Reducer,
-    prepareInitialStateForStore?: (state: any) => any,
-): (state: any, action: AnyAction) => Reducer;
-export function initStateWithPrevTab(store: Store): Store;
-export function initMessageListener(store: Store): Store;
-export function isActionSynced(action: AnyAction): boolean;
+export function withReduxStateSync<A extends Action, S>(
+    appReducer: Reducer<S>,
+    prepareInitialStateForStore?: (state: S) => unknown,
+): (state: S, action: A) => Reducer<S, A>;
+export function initStateWithPrevTab<S>(store: Store<S>): void;
+export function initMessageListener<S>(store: Store<S>): void;
+export function isActionSynced(action: Action): boolean;
